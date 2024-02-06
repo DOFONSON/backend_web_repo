14e4e2aefae08bfbd7d1eb9404760a8a7de1f2f7
D3k1 <dofodofonson@gmail.com>
2024-02-06 12:54:23 +0300

    Footer and title added

--- lab_1/index.php
+++ lab_1/index.php
@@ -3,14 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
     <title>Лабораторная 1</title>
 </head>
 <body>
     <header>
         <img src="img/i.webp" alt="" width=400px>
+        <div class="description">
+            <h2 class="h1">2.1.Домашняя работа: Hello, World!</h2>
+        </div>
     </header>
-    <?php
-        echo 123
-     ?>   
+    <main>
+        <?php
+            echo 'Hello world'
+        ?> 
+    </main>
+     <footer>
+        <h3>Выполнил Назаров Тимофей Дмитриевич</h3>
+     </footer>
 </body>
 </html>
