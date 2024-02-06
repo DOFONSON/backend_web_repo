6df7d0a573e1c9919a50a7eab6f29c73816386a0
D3k1 <dofodofonson@gmail.com>
2024-02-06 12:48:00 +0300

    First commit

---
+++ lab_1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Лабораторная 1</title>
+</head>
+<body>
+    <header>
+        <img src="img/i.webp" alt="" width=400px>
+    </header>
+    <?php
+        echo 123
+     ?>   
+</body>
+</html>
