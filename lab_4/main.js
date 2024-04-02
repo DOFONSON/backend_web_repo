bbb0b52aca265391f0aad5fc110cdc64f8f517f1
D3k1 <dofodofonson@gmail.com>
2024-04-02 21:52:24 +0300

    Js added

---
+++ lab_4/main.js
@@ -0,0 +1,40 @@
+const input = document.getElementById('numbersInput');
+
+function appendToDisplay(content) {
+    input.value += content;
+}
+
+let qwe = document.getElementsByClassName('btnBtn')
+for (const item of qwe) {
+    item.addEventListener('click', () => {
+        input.value += item.textContent
+    })
+}
+console.log(qwe);
+document.getElementById('clearBtn').addEventListener('click', () => {
+    input.value = '';
+})
+
+document.getElementById('calc').addEventListener('click', () => {
+        const problem = input.value;
+        fetch('lab4.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: `problem=${encodeURIComponent(problem)}`
+        })
+        .then(response => response.text())
+        .then(result => {
+            window.location.href = `${window.location.pathname}?result=${encodeURIComponent(result)}`;
+        })
+        .catch(error => console.error('Ошибка:', error));
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const result = urlParams.get('result');
+    if (result) {
+        document.getElementById('input').value = decodeURIComponent(result);
+    }
+});
